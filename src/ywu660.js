681ff2fd366292ea1a0193f926fe5e59a388e76d
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-08-03 11:43:34 +0800

    Improve the change between pages functions. Also adjust the css of the comment page.

--- src/ywu660.js
+++ src/ywu660.js
@@ -1,20 +1,49 @@
+
 function showTap ( id ) {
-    document.getElementById(id).style.display = "block";
+    hideTaps();
+    document.getElementById( id ).style.display = "block";
 }
 
+function hideTaps() {
+    const list = document.getElementsByClassName("tap");
+    for(var i=0;i<list.length;i++) {
+        list[i].style.display = "none";
+    }
+}
 
-function submitSuccessful( innerText ) {
-    document.getElementById( "submitModal").style.display = "block";
+function submitComment( comment ) {
+    fetch("http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(comment)
+    }).then( r => r.json() )
+        .then( result => console.log(result.message) );
+ }
+
+function submitSuccessful() {
+    document.getElementById("submitModal").style.display = "block";
 }
 
 function closeModal() {
-    document.getElementById( "submitModal").style.display = "none";
+    document.getElementById("submitModal").style.display = "none";
+}
+
+function getProducts() {
+    fetch(" http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/items")
+        .then( res => {
+            return res.json()
+        } )
+        .then( data => {
+
+        } );
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    var modal = document.getElementById( "submitModal");
-    if (event.target == modal) {
+window.onclick = function( event) {
+    const modal = document.getElementById("submitModal")
+    if ( event.target == modal ) {
         modal.style.display = "none";
     }
 }
