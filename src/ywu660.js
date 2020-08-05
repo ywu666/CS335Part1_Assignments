b3d1d7b8fe5142ed6d296025524e7198b6640b48
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-08-05 15:52:06 +0800

    Adjust the content of the homepage.

--- src/ywu660.js
+++ src/ywu660.js
@@ -1,37 +1,33 @@
 
 function showTap ( id ) {
     hideTaps();
-    if(id == "productsTap") {
+    if( id === "productsTap" ) {
         getProducts();
     }
+
+    if( id === "newsTap" ) {
+        getNews();
+    }
+
     document.getElementById( id ).style.display = "block";
 }
 
 function hideTaps() {
     const list = document.getElementsByClassName("tap");
-    for(var i=0;i<list.length;i++) {
+    for(var i=0; i<list.length; i++) {
         list[i].style.display = "none";
     }
 }
 
 function submitComment( comment ) {
-    fetch("http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(comment)
-    }).then( r => r.json() )
-        .then( result => console.log(result.message) );
+    fetch("http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/", { method: 'POST' })
+        .then( response =>
+            response.status
+        )
+        .then( data =>
+            console.log( data )
+        );
  }
-
-function submitSuccessful() {
-    document.getElementById("submitModal").style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById("submitModal").style.display = "none";
-}
 
 function getProducts() {
     fetch(" http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/items",{
@@ -50,7 +46,7 @@ function getProducts() {
 
 function showProducts( products ) {
     let tableContent = "";
-    let counter = 0;
+    let count = 0;
 
     for (let i = 0; i < products.length; i++) {
             const record = products[i];
@@ -58,16 +54,36 @@ function showProducts( products ) {
             tableContent += "<td>" +
                 "<img src='http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/itemimg?id=" + record.ItemId + "'/>" +
                 "<h3>" + record.Title + "</h3>" +
-                "<figcaption>"+ record.Origin + " " + record.Price + " " + record.Type +"</figcaption>" +
+                "<figcaption>"+ record.Origin + " Price: $" + record.Price + " " + record.Type +"</figcaption>" +
                 "<button class='buyBtn' value='" + record.ItemID + "'>Buy Now</button>" +
                 "</td>";
+            count += 1;
 
-            counter += 1;
-
-            if (counter > 3) {
+            if (count > 2) {
                 tableContent += "</tr>\n";
-                counter = 0;
+                count = 0;
             }
         }
         document.getElementById("showProducts").innerHTML = tableContent;
 }
+
+function getNews () {
+    fetch(" http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/news",{
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+        .then( response =>
+            response.json()
+        )
+        .then( data => {
+            console.log(data);
+            showNews( data )
+        } );
+
+}
+
+function showNews( news ) {
+
+}
+
