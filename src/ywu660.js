fd56174d146a8dc7fdb59f4ba8c2c06507174c99
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-08-05 15:04:28 +0800

    Show products on the product page.

--- src/ywu660.js
+++ src/ywu660.js
@@ -1,6 +1,9 @@
 
 function showTap ( id ) {
     hideTaps();
+    if(id == "productsTap") {
+        getProducts();
+    }
     document.getElementById( id ).style.display = "block";
 }
 
@@ -31,18 +34,49 @@ function closeModal() {
 }
 
 function getProducts() {
-    fetch(" http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/items")
-        .then( res => {
-            return res.json()
-        } )
+    fetch(" http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/items",{
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+        .then( response =>
+            response.json()
+        )
         .then( data => {
-
+            console.log(data);
+            showProducts( data )
         } );
+}
+
+
+function showProducts( products ) {
+    let tableContent = "";
+    let counter = 0;
+
+    for (let i = 0; i < products.length; i++) {
+            const record = products[i];
+
+            tableContent += "<td>" +
+                "<img src='http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/itemimg?id=" + record.ItemId + "'/>" +
+                "<figcaption>" + record.Title + "</figcaption>" +
+                "<figcaption>"+ record.Origin + " " + record.Price + " " + record.Type +"</figcaption>" +
+                "<button class='buyBtn' value='" + record.ItemID + "'>Buy Now</button>" +
+                "</td>";
+
+            counter += 1;
+
+            if (counter > 3) {
+                tableContent += "</tr>\n";
+                counter = 0;
+            }
+        }
+        document.getElementById("showProducts").innerHTML = tableContent;
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function( event) {
-    const modal = document.getElementById("submitModal")
+    const modal = document.getElementById("submitModal");
+
     if ( event.target == modal ) {
         modal.style.display = "none";
     }
