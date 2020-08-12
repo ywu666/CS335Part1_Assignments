564a3dd239fb25a9dcc42df6b0a602388ffab409
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-08-12 19:44:27 +0800

    Add space to the code.

--- Assignment1-2/src/ywu660.js
+++ Assignment1-2/src/ywu660.js
@@ -1,5 +1,5 @@
 function getData() {
-    fetch ("http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/g", {
+    fetch ( "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/g", {
         headers: {
             'Accept': 'application/json'
         },
@@ -17,15 +17,15 @@ function generateCircles( data ) {
     //Set up the variables
     let positions = [];
     const numOfVertices = data.length;
-    let circles = makePolygon(250,125, numOfVertices, 100 );
-    let innerCircles = makePolygon( 250,125, numOfVertices, 80 );
+    let circles = makePolygon( 250, 125, numOfVertices, 100 );
+    let innerCircles = makePolygon( 250, 125, numOfVertices, 80 );
     for( let i = 0; i < numOfVertices; i++ ) {
         //Create an inner cycle for lines to connected vertexes
         positions.push( innerCircles[ i ].x + "," + innerCircles[ i ].y );
     }
 
     //Draw the n-side polygon with text circles
-    let content = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 250\">";
+    let content = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 500 250'>";
     for( let i = 0; i < circles.length; i++ ) {
         //Center the text in the circle
         content += "<circle cx='"+ circles[ i ].x +"' cy='"+ circles[ i ].y +"' fill='none' stroke='black' stroke-width='1px' r='20'/>" +
@@ -36,8 +36,8 @@ function generateCircles( data ) {
     for( let i = 0; i < numOfVertices; i++ ) {
         for( let j = i; j < numOfVertices; j++ ) {
             if ( data[ i ][ j ] == 1 ) {
-                content += "<path d='M"+ positions[ i ] +" L"+ positions[j] +"'" +
-                    "style='stroke: black; stroke-width: 1px; fill: none;'/>";
+                content += "<path d='M"+ positions[ i ] +" L"+ positions[ j ] +"'" +
+                    "style='stroke:black; stroke-width:1px; fill:none;'/>";
             }
         }
 
@@ -56,8 +56,8 @@ function generateCircles( data ) {
  * @returns {[]} the x and y positions of the points
  */
 function makePolygon(posX, posY, numSides, size) {
-    let points = [], point={};
-    for (let i = 0; i < numSides; i++) {
+    let points = [], point = {};
+    for ( let i = 0; i < numSides; i++ ) {
         point = {
             x: posX + size * Math.sin( 2 * Math.PI * i / numSides ),
             y: posY + size * Math.cos( 2 * Math.PI * i / numSides ),
