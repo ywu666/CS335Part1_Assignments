18e976e5aa03da10d1dad2889b101645ce7a1359
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-08-12 19:32:26 +0800

    Create an inner cycle for connected vertexes.

--- Assignment1-2/src/ywu660.js
+++ Assignment1-2/src/ywu660.js
@@ -17,20 +17,19 @@ function generateCircles( data ) {
     //Set up the variables
     const numOfVertices = data.length;
     let positions = [];
-    let circles = makePolygon(250,150, numOfVertices, 80 );
+    let circles = makePolygon(250,125, numOfVertices, 80 );
+    let innerCircles = makePolygon(250,125, numOfVertices, 60);
     for( let i = 0; i < numOfVertices; i++ ) {
-        positions.push( circles[ i ].x + "," + circles[ i ].y );
-
+        //Create an inner cycle for lines to connected vertexes
+        positions.push( innerCircles[ i ].x + "," + innerCircles[ i ].y );
     }
-    console.log( circles );
-    console.log( positions );
 
-    //Draw the n-side polygon
-    let content = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 300\">";
+    //Draw the n-side polygon with text circles
+    let content = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 250\">";
     for( let i = 0; i < circles.length; i++ ) {
         //Center the text in the circle
-        content += "<circle cx='"+ circles[ i ].x +"' cy='"+ circles[ i ].y +"' fill='black' stroke='black' stroke-width='1px' r='20'/>" +
-            "<text x='"+ circles[ i ].x +"' y='"+ circles[ i ].y +"' text-anchor='middle' dy='0.3em' fill='white'>"+ i +"</text>";
+        content += "<circle cx='"+ circles[ i ].x +"' cy='"+ circles[ i ].y +"' fill='none' stroke='black' stroke-width='1px' r='20'/>" +
+            "<text x='"+ circles[ i ].x +"' y='"+ circles[ i ].y +"' text-anchor='middle' dy='0.3em'>"+ i +"</text>";
     }
 
     //Connected the vertices based on the metrics
@@ -48,10 +47,17 @@ function generateCircles( data ) {
     document.getElementById( "graph" ).innerHTML = content;
 }
 
-
+/**
+ * Generate n-side regular polygon
+ * @param posX the x of reference center
+ * @param posY the y of reference center
+ * @param numSides
+ * @param size the radius of a cycle
+ * @returns {[]} the x and y positions of the points
+ */
 function makePolygon(posX, posY, numSides, size) {
-    var points = [], point={};
-    for (var i = 0; i < numSides; i++) {
+    let points = [], point={};
+    for (let i = 0; i < numSides; i++) {
         point = {
             x: posX + size * Math.sin( 2 * Math.PI * i / numSides ),
             y: posY + size * Math.cos( 2 * Math.PI * i / numSides ),
